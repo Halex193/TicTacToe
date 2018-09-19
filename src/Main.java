091929d4fff5bbbe23059d0856381d1ce7eba7e9
Horațiu Udrea <udrea.horatiu@gmail.com>
2018-09-19 13:09:10 +0300

    7.08

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main
 {
 
@@ -6,25 +8,47 @@ public class Main
             {' ', 'X', ' '},
             {' ', ' ', ' '}
     };
+    static final int dim = 3;
 
     public static void main(String[] args)
     {
         printBoard();
+        int x = -1, y = -1;
+        Scanner s = new Scanner(System.in);
+        while (!isValid(x, y))
+        {
+            System.out.print("X: ");
+            x = s.nextInt();
+            System.out.print("Y: ");
+            y = s.nextInt();
+        }
+        signs[y][x] = 'X';
+        printBoard();
+    }
+
+    private static boolean isValid(int x, int y)
+    {
+        return between(x, 0, dim) && between(y, 0, dim) && signs[y][x] == ' ';
+    }
+
+    private static boolean between(int n, int min, int max)
+    {
+        return n >= min && n < max;
     }
 
     public static void printBoard()
     {
-        for (int i = 0; i < signs.length; i++)
+        for (int i = 0; i < dim; i++)
         {
-            for (int j = 0; j < signs[0].length; j++)
+            for (int j = 0; j < dim; j++)
             {
                 System.out.print(" " + signs[i][j] + " ");
-                if (j != signs[0].length - 1)
+                if (j != dim - 1)
                     System.out.print("|");
             }
 
             System.out.println();
-            if (i != signs.length - 1)
+            if (i != dim - 1)
                 System.out.println("-----------");
 
         }

3373929059807df9ddbd2bab6442918365ebd329
Horațiu Udrea <udrea.horatiu@gmail.com>
2018-09-19 13:02:13 +0300

    7.07

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,32 @@
-public class Main {
+public class Main
+{
 
-    public static void main(String[] args) {
-	// write your code here
+    static char[][] signs = new char[][]{
+            {'O', ' ', ' '},
+            {' ', 'X', ' '},
+            {' ', ' ', ' '}
+    };
+
+    public static void main(String[] args)
+    {
+        printBoard();
+    }
+
+    public static void printBoard()
+    {
+        for (int i = 0; i < signs.length; i++)
+        {
+            for (int j = 0; j < signs[0].length; j++)
+            {
+                System.out.print(" " + signs[i][j] + " ");
+                if (j != signs[0].length - 1)
+                    System.out.print("|");
+            }
+
+            System.out.println();
+            if (i != signs.length - 1)
+                System.out.println("-----------");
+
+        }
     }
 }
